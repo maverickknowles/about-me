876a61bde88dcd9a54c10cc4e260132d0d3a800d
maverickk4 <124333912+maverickk4@users.noreply.github.com>
2023-02-07 02:57:32 -0500

    big update

--- about-me.js
+++ about-me.js
@@ -3,13 +3,57 @@ document.addEventListener("mousemove", function(event) {
     var y = event.clientY / window.innerHeight - 0.25;
     
     document.body.style.backgroundPosition = x * 10 + "px " + y * 10 + "px";
-});
-
-document.addEventListener("mousemove", function(event) {
+  });
+  
+  document.addEventListener("mousemove", function(event) {
     let x = event.clientX;
     let y = event.clientY;
     let elements = document.querySelectorAll("h1, h2, .about-me *");
     elements.forEach(function(element) {
       element.style.transform = `translate(${-x / 100}px, ${-y / 100}px)`;
     });
-});
+  });
+  const photoUrls = [
+    "photos/photo1.jpg",
+    "photos/photo2.jpg",
+    "photos/photo3.jpg",
+    "photos/photo4.jpg",
+    "photos/photo5.jpg",
+    "photos/photo6.jpg",
+    "photos/photo7.jpg",
+    "photos/photo8.jpg",
+    "photos/photo9.jpg",
+    "photos/photo10.jpg",
+    "photos/photo11.jpg",
+    "photos/photo12.jpg",
+    "photos/photo13.jpg",
+    "photos/photo14.jpg",
+    "photos/photo15.jpg",
+    "photos/photo16.jpg",
+    "photos/photo17.jpg",
+    "photos/photo18.jpg",
+    "photos/photo19.jpg",
+    "photos/photo20.jpg",
+    "photos/photo21.jpg",
+    "photos/photo22.jpg",
+    "photos/photo23.jpg",
+    "photos/photo24.jpg",
+    "photos/photo25.jpg",
+    "photos/photo26.jpg",
+    "photos/photo27.jpg",
+    "photos/photo28.jpg",
+    "photos/photo29.jpg",
+    "photos/photo30.jpg",
+    "photos/photo31.jpg",
+    "photos/photo32.jpg",
+    "photos/photo33.jpg",
+    "photos/photo34.jpg",
+    "photos/photo35.jpg",
+    "photos/photo36.jpg",
+    "photos/photo37.jpg",
+    "photos/photo38.jpg",
+    "photos/photo39.jpg",
+    "photos/photo40.jpg",
+    "photos/photo41.jpg"
+    ];
+    photoUrls.src
